316f4b1544f5638b089eca1434310f89c337a4dd
brieu2332 <gabriel2332rodrigues@gmail.com>
2024-07-29 09:45:20 -0300

    primeiro commit com esqueleto do codigo

---
+++ teste.py
@@ -0,0 +1,17 @@
+from datetime import date #para saber a data atual
+
+#variaveis
+anoAtual = date.today().year #coloca o ano atual dentro da vari ano
+nome = ""
+anoDeNascimento = 0
+calculoIdade = 0
+
+#entrada
+nome = input('diga seu nome:')
+anoDeNascimento = int(input('coloque sua data de nascimento:'))
+
+#mostra e calcula idade
+calculoIdade = anoAtual - anoDeNascimento
+print(calculoIdade)
+
+

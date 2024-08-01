3f82247a8da66ab2aefefca3fd22ad648c6c8b2b
brieu2332 <gabriel2332rodrigues@gmail.com>
2024-08-01 13:07:34 -0300

    teste de persistencia de classe

--- teste.py
+++ teste.py
@@ -1,19 +1,28 @@
-from datetime import date #para saber a data atual
+import json
 
-#voce esta na branch gabriel
+class MyClass:
+    def __init__(self, name, value, cpf):
+        self.name = name
+        self.value = value
+        self.cpf = cpf
 
-#variaveis
-anoAtual = date.today().year #coloca o ano atual dentro da vari ano
-nome = ""
-anoDeNascimento = 0
-calculoIdade = 0
+    def to_dict(self):
+        return {'name': self.name, 'value': self.value, 'cpf':self.cpf}
 
-#entrada
-nome = input('diga seu nome:')
-anoDeNascimento = int(input('coloque sua data de nascimento:'))
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data['name'], data['value'], data['cpf'])
 
-#mostra e calcula idade
-calculoIdade = anoAtual - anoDeNascimento
-print(calculoIdade)
+# Criando uma instância da classe
+obj = MyClass('example', 42, 123)
 
+# Salvando o objeto em um arquivo JSON
+with open('myclass.json', 'w') as f:
+    json.dump(obj.to_dict(), f)
 
+# Carregando o objeto do arquivo JSON
+with open('myclass.json', 'r') as f:
+    data = json.load(f)
+    loaded_obj = MyClass.from_dict(data)
+
+print(loaded_obj.name, loaded_obj.value, loaded_obj.cpf)

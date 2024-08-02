b625444a624e09afcf973f6dd626d370ac1011c9
José Mateus Amaral de Miranda <50233573+josemateusamaral@users.noreply.github.com>
2024-08-01 22:29:40 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,21 +1,6 @@
-from datetime import date 
 import json# importa json para persistir o dado em json
-
-# class definition ---------------------
-class Person:
-    def __init__(self, name, year_born, cpf):
-        self.name = name
-        self.year_born = year_born
-        self.cpf = cpf
-
-    def __str__(self):
-        return f'''{self.name}, born in {self.year_born} (age: {date.today().year-self.year_born}), 
-CPF: {self.cpf}'''
-    
-    def toDict(self):
-        return {'name': self.name, 'year_born': self.year_born, 'cpf': self.cpf}#passa quais informações serao salvas
-    
-   
+from Person import Person
+  
 # main program -------------------------
 
 # ask data
